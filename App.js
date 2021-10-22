09c0baa2f4040ff8682625bd38aa27832c7ee66a
ChenWarren <72281162+ChenWarren@users.noreply.github.com>
2021-10-22 16:13:04 -0700

    init

--- App.js
+++ App.js
@@ -1,21 +1,84 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Platform, SafeAreaView, StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
+
+import Color from './config/Color';
 
 export default function App() {
+  const [searchTask, setSearchTask] = useState()
+  const [ task, setTask ] = useState('')
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.title}>
+        <Text style={{fontWeight: 'bold', fontSize: 24, paddingVertical: 10, color: Color.secondary,}}>TASK LIST</Text>
+        <Button color={Color.active} title={'Add'}/>
+      </View>
+      <View style={styles.inputWrapper}>
+        <AntDesign name="search1" size={18} color={Color.disative} style={{paddingTop: 10, paddingLeft: 10}} />
+        <TextInput
+          placeholder={'Search'}
+          placeholderTextColor={Color.disative}
+          onChange={setSearchTask}
+          value={searchTask}
+          style={styles.searchInput}
+        />
+      </View>
+      <View style={styles.inputWrapper}>
+        <TextInput
+          placeholder='Enter task'
+          placeholderTextColor={Color.disative}
+          onChange={setTask}
+          value={task}
+          style={styles.taskInput}
+        />
+
+      </View>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  btnWrapper: {
+    height: 40,
+    backgroundColor: Color.active,
+    borderRadius: 5,
+    margin: 10
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: Color.primary,
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+    alignItems: 'center'
   },
+  inputWrapper: {
+    width: '95%',
+    flexDirection: 'row',
+    margin: 10,
+    padding: 10,
+    backgroundColor: Color.pageBG,
+    borderRadius: 10,
+  },
+  searchInput: {
+    margin: 10,
+    color: Color.secondary,
+  },
+  taskInput: {
+    width: '95%',
+    height: 40,
+    margin: 10,
+    color: Color.secondary,
+    borderBottomWidth: 1,
+    borderColor: Color.disative,
+  },
+  title:{
+    height: 45,
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'baseline',
+    justifyContent: 'space-between',
+    paddingHorizontal: 15,
+  }
 });
---
+++ config/Color.js
@@ -0,0 +1,7 @@
+export default {
+    primary: '#000',
+    secondary: '#fff',
+    disative: '#6e6e6e',
+    pageBG: '#1f1f1f',
+    active: '#bf7aff',
+}
