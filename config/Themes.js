570eeedbd04e96dfac24db749b8d781469a785e9
ChenWarren <72281162+ChenWarren@users.noreply.github.com>
2021-11-01 17:14:30 -0700

    themes color

---
+++ config/Themes.js
@@ -0,0 +1,18 @@
+export default {
+    darkmode: {
+        primary: '#000',
+        secondary: '#fff',
+        disative: '#6e6e6e',
+        pageBG: '#1f1f1f',
+        active: '#bf7aff',
+        altactive: '#8a68a6',
+    },
+    lightmode: {
+        primary: '#fff',
+        secondary: '#000',
+        disative: '#ececec',
+        pageBG: '#000',
+        active: '#bf7aff',
+        altactive: '#dab2ff',
+    }
+}
